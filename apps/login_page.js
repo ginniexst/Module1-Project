b5ef3ddf4eae30d348fb6cc4ba9e7dc2f3f71c94
Ginnie <ginniexst@gmail.com>
2024-10-15 23:15:42 +0900

    Add files via upload

--- apps/login_page.js
+++ apps/login_page.js
@@ -1,8 +1,27 @@
 // Lấy nút đăng nhập
 let loginBtn = document.getElementById("loginBtn");
+let accList = JSON.parse(localStorage.getItem("accList"));
 
-// Lấy email và pw nhập vào
-let loginEmail = document.getElementById("loginEmail");
-let loginPw = document.getElementById("loginPw");
+loginBtn.onclick = function() {
+    // Lấy email và pw nhập vào
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPw").value;
 
-// So sánh với localstorage
+    // Tìm tài khoản xem có tồn tại hay không
+    let find = accList.find(function(element, index) {
+        return element.email === email;
+    });    
+
+    if (!find) {
+        alert(`Không có tài khoản với email: ${email} trong hệ thống`)
+    } else {
+        if (find.pw !== password) {
+            alert("Mật khẩu không đúng");
+        } else {
+            alert("Đăng nhập thành công");
+            location.href = "http://127.0.0.1:5500/home_page.html"
+        }
+    };
+}
+
+
