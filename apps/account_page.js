addec0c18ca6b54b7803895b3adcea4a4cdfb3da
Ginnie <ginniexst@gmail.com>
2024-10-15 17:05:05 +0900

    Add files via upload

---
+++ apps/account_page.js
@@ -0,0 +1,6 @@
+// Đăng xuất
+let logoutBtn = document.getElementById("logOutBtn");
+
+logoutBtn.onclick = function () {
+    location.href = "http://127.0.0.1:5500/login_page.html#";
+};
---
+++ apps/login_page.js
@@ -0,0 +1,8 @@
+// Lấy nút đăng nhập
+let loginBtn = document.getElementById("loginBtn");
+
+// Lấy email và pw nhập vào
+let loginEmail = document.getElementById("loginEmail");
+let loginPw = document.getElementById("loginPw");
+
+// So sánh với localstorage
