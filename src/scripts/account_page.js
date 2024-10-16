d70db815259022b78d8d814fb39d81967373766e
Ginnie <ginniexst@gmail.com>
2024-10-16 12:00:42 +0900

    Add files via upload

--- src/scripts/account_page.js
+++ src/scripts/account_page.js
@@ -1,6 +1,49 @@
+<<<<<<< HEAD
 // Đăng xuất
 let logoutBtn = document.getElementById("logOutBtn");
 
 logoutBtn.onclick = function () {
     location.href = "http://127.0.0.1:5500/login_page.html#";
 };
+
+// Render thông tin
+let accountList = JSON.parse(localStorage.getItem("accountList"));
+let tbody = document.getElementById("tbody");
+
+function render() {
+    for (let account of accountList) {
+        let html = "";
+        let status = "";
+        if (account.status) {
+            status = "Đang hoạt động"
+        } else {
+            status = "Đang bị khoá"
+        }
+        html = `
+            <tr>
+                <td>${account.id}</td>
+                <td>${account.email}</td>
+                <td>${account.password}</td>
+                <td>${account.name}</td>
+                <td>${status}</td>
+                <td>
+                    <span class="button button-edit">Khoá</span>
+                </td>
+                <td>
+                    <span class="button button-delete">Mở khoá</span>
+                </td>
+            </tr>
+        `;
+        tbody.innerHTML += html;
+    }
+};
+
+render();
+=======
+// Đăng xuất
+let logoutBtn = document.getElementById("logOutBtn");
+
+logoutBtn.onclick = function () {
+    location.href = "http://127.0.0.1:5500/login_page.html#";
+};
+>>>>>>> remotes/origin/main
--- src/scripts/class_page.js
+++ src/scripts/class_page.js
@@ -1,3 +1,45 @@
+<<<<<<< HEAD
+
+let logoutBtn = document.getElementById("logOutBtn");
+let addBtn = document.getElementById("addBtn");
+
+// Đăng xuất
+logoutBtn.onclick = function() {
+    location.href = "http://127.0.0.1:5500/login_page.html#";
+};
+
+// Render thông tin
+let classList = JSON.parse(localStorage.getItem("classList"));
+let tbody = document.getElementById("tbody");
+
+function render() {
+    for (let cl of classList) {
+        let html = "";
+        html = `
+            <tr>
+                <td>${cl.id}</td>
+                <td>${cl.code}</td>
+                <td>${cl.name}</td>
+                <td>${cl.teacher}</td>
+                <td>${cl.description}</td>
+                <td>${cl.studentNumber}</td>
+                <td>${cl.status}</td>
+                <td>${cl.course}</td>
+                <td>
+                    <span class="button button-edit">Sửa</span>
+                </td>
+                <td>
+                    <span class="button button-delete">Xoá</span>
+                </td>
+            </tr>
+        `;
+        tbody.innerHTML += html;
+    }
+};
+
+render();
+
+=======
 
 let logoutBtn = document.getElementById("logOutBtn");
 let addBtn = document.getElementById("addBtn");
@@ -11,3 +53,4 @@ addBtn.onclick = function() {
     location.href = "http://127.0.0.1:5500/class_page_1.html#"
 };
 
+>>>>>>> remotes/origin/main
