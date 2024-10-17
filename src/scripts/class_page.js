9b83af91a69ac1346a2eca017bff10d0fe3fad16
Ginnie <ginniexst@gmail.com>
2024-10-17 22:14:04 +0900

    no message

--- src/scripts/class_page.js
+++ src/scripts/class_page.js
@@ -4,21 +4,18 @@ let tbody = document.getElementById("tbody");
 let addBtn = document.getElementById("addBtn");
 let addFormDiv = document.getElementById("addFormDiv");
 let span = document.getElementsByClassName("close")[0];
-console.log(span);
-
-let closeClass = document.getElementById("closeClass");
+let closeAdd = document.getElementById("closeAdd");
 let addClass = document.getElementById("addClass");
 let addForm = document.getElementById("addForm");
 
 // Đăng xuất
 logoutBtn.onclick = function() {
-    location.href = "http://127.0.0.1:5500/login_page.html#";
+    location.href = "http://127.0.0.1:5500/logout_page.html#";
 };
 
 // Render thông tin
-
-
 function render() {
+    tbody.innerHTML = "";
     for (let cl of classList) {
         let html = "";
         html = `
@@ -41,18 +38,96 @@ function render() {
         `;
         tbody.innerHTML += html;
     }
+    // Thêm lớp học
+    addForm.onsubmit = function(event) {
+        event.preventDefault();
+
+        let code = addForm.classCode.value;
+        let name = addForm.className.value;
+        let num = addForm.classNumber.value;
+        let statusNum = addForm.classStatus.value;
+
+        let findCode = classList.find(element => element.code === code);
+        let findName = classList.find(element => element.name === name);
+
+        let status = "";
+        if (statusNum == 1) {
+            status = "Chờ lớp";
+        } else if (statusNum == 2) {
+            status = "Hoạt động";
+        } else {
+            status = "Kết thúc";
+        };
+
+        if (findCode) {
+            alert(`Mã khóa học ${code} đã tồn tại`);
+        } else {
+            if (findName) {
+                alert(`Tên khóa học ${name} đã tồn tại`);
+            } else {
+                if (num == 0) {
+                    alert(`Sỉ số lớp học phải lớn hơn 0`);
+                } else {
+                    let newClass = {
+                        id: classList.length + 1,
+                        code: code,
+                        name: name,
+                        teacher: addForm.classTeacher.value,
+                        description: addForm.classDescription.value,
+                        studentNumber: num,
+                        status: status,
+                        course: ""
+                    }
+                    classList.push(newClass);
+                    localStorage.setItem("classList", JSON.stringify(classList));
+                    addForm.classCode.value = "";
+                    addForm.className.value = "";
+                    addForm.classTeacher.value = "";
+                    addForm.classDescription.value = "";
+                    addForm.classNumber.value = "";
+                    addFormDiv.style.display = "none";
+                    render();
+                }
+            }
+        }
+    }
 };
 
 render();
 
+// Mở modal thêm lớp học
 addBtn.onclick = function () {
     addFormDiv.style.display = "block";
 };
 
+// Đóng modal thêm lớp học
 span.onclick = function () {
     addFormDiv.style.display = "none";
 };
-
-closeClass.onclick = function () {
+closeAdd.onclick = function () {
     addFormDiv.style.display = "none";
 };
+
+// Đóng modal cập nhật lớp học
+let editFromDiv = document.getElementById("editFormDiv");
+let editSpan = document.getElementById("editSpan");
+let closeEdit = document.getElementById("closeEdit");
+
+closeEdit.onclick = function() {
+    editFormDiv.style.display = "none";
+};
+editSpan.onclick = function() {
+    editFormDiv.style.display = "none";
+}
+
+// Đóng modal xác nhận xóa
+let delFormDiv = document.getElementById("delFormDiv");
+let delSpan = document.getElementById("delSpan");
+let noBtn = document.getElementById("noBtn");
+
+delSpan.onclick = function() {
+    delFormDiv.style.display = "none";
+};
+noBtn.onclick = function() {
+    delFormDiv.style.display = "none";
+}
