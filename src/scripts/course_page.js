2f09425e0c345c9fb0a58331644e9142267c96a4
Ginnie <ginniexst@gmail.com>
2024-10-17 15:26:00 +0900

    update

--- src/scripts/course_page.js
+++ src/scripts/course_page.js
@@ -8,14 +8,15 @@ let span = document.getElementsByClassName("close")[0];
 let closeCourse = document.getElementById("closeCourse");
 let addCourse = document.getElementById("addCourse");
 let addForm = document.getElementById("addForm");
+let delCourse = document.getElementById("delCourse");
 
 
+// Đăng xuất
 logoutBtn.onclick = function () {
     location.href = "http://127.0.0.1:5500/logout_page.html";
 };
 
 // Render thông tin
-
 function render() {
     tbody.innerHTML = "";
     for (let course of courseList) {
@@ -34,29 +35,34 @@ function render() {
                 <td>${course.courseTime}</td>
                 <td>${status}</td>
                 <td>
-                    <span class="button button-edit">Sửa</span>
+                    <span class="button button-edit" id="editCourse">Sửa</span>
                 </td>
                 <td>
-                    <span class="button button-delete">Xoá</span>
+                    <span class="button button-delete" id="delCourse">Xoá</span>
                 </td>
             </tr>
         `;
         tbody.innerHTML += html;
     };
 
-    // add new course
+    // Thêm khoá học mới
     addForm.onsubmit = function (event) {
         event.preventDefault();
 
         let code = addForm.courseCode.value;
         let name = addForm.courseName.value;
         let time = addForm.courseTime.value;
-
         let findCode = courseList.find(element => element.code === code);
+        let findName = courseList.find(element => element.courseName === name);
+        let statusValue = addForm.courseStatus.value;
+        let stt = new Boolean(true);
+        if (statusValue == 0) {
+            stt = false;
+        }
+
         if (findCode) {
             alert(`Mã khoá học ${code} đã tồn tại`);
         } else {
-            let findName = courseList.find(element => element.courseName === name);
             if (findName) {
                 alert(`Tên khoá học ${name} đã tồn tại`);
             } else {
@@ -68,7 +74,7 @@ function render() {
                         code: addForm.courseCode.value,
                         courseName: addForm.courseName.value,
                         courseTime: addForm.courseTime.value,
-                        status: true
+                        status: stt
                     };
                     courseList.push(newCourse);
                     localStorage.setItem("courseList", JSON.stringify(courseList));
@@ -78,10 +84,14 @@ function render() {
                     addForm.courseTime.value = "";
                     render();
                 }
-            };
-        };
-
+            }
+        }
     };
+
+    // Xoá khoá học
+    delCourse.onclick = function() {
+        
+    }
 };
 
 render();
@@ -97,34 +107,3 @@ span.onclick = function () {
 closeCourse.onclick = function () {
     addFormDiv.style.display = "none";
 };
-
-// Sắp xếp tăng dần
-// let ascending = document.getElementById("sortAscending");
-// ascending.onclick = function () {
-//     let sortAscending = courseList.slice(0);
-//     sortAscending.sort(function (a, b) {
-//         let x = a.code.toLowerCase();
-//         let y = b.code.toLowerCase();
-//         return x < y ? -1 : x > y ? 1 : 0;
-//     });
-//     localStorage.setItem(JSON.stringify("courseList", sortAscending));
-//     render();
-// };
-
-
-// Sắp xếp giảm dần
-// let descending = document.getElementById("sortDescending");
-// descending.onclick = function () {
-//     let sortDescending = courseList.slice(0);
-//     sortDescending.sort(function (a, b) {
-//         let x = a.code.toLowerCase();
-//         let y = b.code.toLowerCase();
-//         return x > y ? -1 : x < y ? 1 : 0;
-//     });
-//     console.log(sortDescending);
-
-//     localStorage.setItem("courseList", JSON.stringify(sortDescending));
-//     render();
-// }
-
-
